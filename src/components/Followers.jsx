4ae2d3e3b194dac52c495a537c574bbe85262472
William Park <william.h.park@gmail.com>
2021-05-05 11:57:53 -0700

    Finished step 4

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -3,12 +3,35 @@ import React from "react";
 class Followers extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      followers: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      `https://api.github.com/users/${this.props.params.username}/followers`
+    )
+      .then((response) => response.json())
+      .then((followers) => {
+        // How can we use `this` inside a callback without binding it??
+        // Make sure you understand this fundamental difference with arrow functions!!!
+        console.log(followers);
+        this.setState({
+          followers: followers,
+        });
+      });
   }
 
   render() {
     return (
       <div className="followers-page">
         <h3>Followers of USERNAME</h3>
+        <ul>
+          {this.state.followers.map(function (follower) {
+            return <li>{follower.login}</li>;
+          })}
+        </ul>
       </div>
     );
   }
